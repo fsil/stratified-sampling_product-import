ba6cbbf822bccaefea33a235bac075a7b125f2da
fsil <69916101+fsil@users.noreply.github.com>
2021-05-10 19:36:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ import pandas as pd
 df = pd.read_csv("C:\\Users\\ACER\\Desktop\\strata\\pop.csv", low_memory=False)
 df_unique = df.drop_duplicates(subset=['vin_or_lp'], keep='first', inplace=False)
 
-N=10
+N = 10
 
 print(df_unique)
 
-goal = df_unique.groupby(['product', 'import'], group_keys=False).apply(lambda x: x.sample(int(np.rint(N*len(x)/len(df_unique))))).sample(frac=1).reset_index(drop=True)
-print(goal)
-goal.to_csv (r'C:\Users\ACER\Desktop\export_dataframe.csv', index=False, header=True)
+vhr_zerokm = df_unique[(df_unique["product"] == "VHR") & (df_unique["lastodometerreading"] == 0)]
+icr_zerokm = df_unique[(df_unique["product"] == "ICR") & (df_unique["lastodometerreading"] == 0)]
+vhr_zeroage = df_unique[(df_unique["product"] == "VHR") & (df_unique["vehicleage"] == 0)]
+icr_zeroage = df_unique[(df_unique["product"] == "ICR") & (df_unique["vehicleage"] == 0)]
+
+final = df_unique.groupby(['product', 'import'], group_keys=False).apply(lambda x: x.sample(int(np.rint(N*len(x)/len(df_unique))))).sample(frac=1).reset_index(drop=True)
+print(final)
+final.to_csv(r'C:\Users\ACER\Desktop\export_dataframe.csv', index=False, header=True)
